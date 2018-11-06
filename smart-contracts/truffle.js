5bc4f79fc4bb8067f845953d6c169a2a7b233b74
linked0 <linked0@gmail.com>
2018-11-06 10:42:49 +0900

    ERC721TokenTest.js 테스트 성공

--- smart-contracts/truffle.js
+++ smart-contracts/truffle.js
@@ -17,12 +17,9 @@ module.exports = {
   // to customize your Truffle configuration!
 	networks: {
 		development: {
-			host: "127.0.0.1",
+			host: "localhost",
 			port: 8545,
 			network_id: "*",
-			from: "0x8790d4A448873CE0F624E31aC732c06eB211a715",
-      		gas: 2000000,
-      		gasPrice: 10000000000,
 		}
 	}
 };
